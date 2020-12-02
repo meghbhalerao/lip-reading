62ad79094d703e761f31359ea67cf5419feb05f8
Megh <33202960+meghbhalerao@users.noreply.github.com>
2020-12-02 11:18:18 +0530

    Update train.py

--- train.py
+++ train.py
@@ -47,23 +47,42 @@ def visual_CNN(input):
     
     return dense2
 
+def audio_CNN(input):
+    #Layer1
+    conv1 = tf.layers.conv3d(input, filters = 16, kernel_size = [3,3,3], strides=[ 1, 1, 1], padding="SAME",activation = tf.nn.leaky_relu, use_bias = True)
+    conv1 = tf.layers.max_pooling3d(conv1,pool_size = [1,3,3],strides = [1,2,2])
+
+    conv2 = tf.layers.conv3d(conv1, filters= 32, kernel_size=[3, 3, 3], strides=[1, 1, 1], padding="SAME",activation=tf.nn.leaky_relu, use_bias=True)
+    conv2 = tf.layers.max_pooling3d(conv2, pool_size=[1, 3, 3], strides=[1, 2, 2])
+
+    conv3 = tf.layers.conv3d(conv2, filters=64, kernel_size=[3, 3, 3], strides=[1, 1, 1], padding="SAME", activation=tf.nn.leaky_relu, use_bias=True)
+    conv3 = tf.layers.max_pooling3d(conv3, pool_size=[1, 3, 3], strides=[1, 2, 2])
+
+    conv4 = tf.layers.conv3d(conv3, filters=128, kernel_size=[3, 3, 3], strides=[1, 1, 1], padding="SAME", activation=tf.nn.leaky_relu, use_bias=True)
+
+    flattening = tf.reshape(conv4,[-1,1])
+    
+    dense1 = tf.contrib.layers.fully_connected(flattening,1000,activation_fn = tf.nn.leaky_relu)
+    #dropout = tf.layers.dropout(inputs = dense1,rate = keep_rate,training = True)
+    dense2 = tf.contrib.layers.fully_connected(dense1,500,activation_fn = tf.nn.leaky_relu)
+    
+    
+    return dense2
 
 def train(x_train,y_train,learning_rate = 0.05, epochs = 10):
-    prediction = visual_CNN(x_input)
-    cost = tf.losses.mean_squared_error(y_input,prediction)
+    prediction_visual = visual_CNN(x_input)
+    prediction_audio = audio_CNN(x_input)
+    cost_v = tf.losses.mean_squared_error(y_input,prediction_visual)
+    cost_a = tf.losses.mean_squared_error(y_input,prediction_audio)
+    cost = cost_a + cost_v
     optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
-
     iterations = len(x_train)
-
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-
         iterations = len(x_train)
-
         for epoch in range(epochs):
             print("Epochs", epoch, "started")
             epoch_loss = 0
-
             for itr in range(iterations):
                   temp = x_train[itr,:,:,:,:] 
                   temp.reshape(1,9,60,100,1)
